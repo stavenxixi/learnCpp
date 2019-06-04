0e3ee5255e57417cfc81511afe09c670e98f2480
steven <1014683756@qq.com>
2019-06-04 16:22:14 +0800

    add files

---
+++ cppLearn/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string line;
+    string word = "word";
+    string hello = "hello";
+    string ss = hello + word;
+
+    cout << word.size();
+    cout << ss;
+    return 0;
+}
